b27ac07caf7d54a1679927faf483284def7db756
Gary DeReese <garydereese@sbcglobal.net>
2021-06-29 11:40:37 -0500

    added grammar input, load and validation

--- src/App.js
+++ src/App.js
@@ -1,115 +1,91 @@
-import React from 'react';
-import { CommandBar, MessageBar, MessageBarType, Pivot, PivotItem, TextField } from '@fluentui/react';
+import React, { useState } from 'react';
+import { CommandBar, FontIcon, getTheme, MessageBar, MessageBarType, Pivot, PivotItem, Spinner, Text, TextField } from '@fluentui/react';
 
 import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this._developCommandItems = [
-      {
-        key: 'load',
-        text: 'Load...',
-        iconProps: { iconName: 'Upload' },
-        onClick: () => this._promptGrammarFile()
-      },
-      {
-        key: 'validate',
-        text: 'Validate',
-        onClick: () => this._validateGrammar()
-      }
-    ];    
+function App(props) {
+  const [grammar, setGrammar] = useState({});
 
-    this.state = {
-      grammar: {
-        error: null,
-        text: ''
-      }
-    };
-  }
+  const theme = getTheme();
+  const styles = {
+    navigationBar: {
+      backgroundColor: theme.palette.themePrimary
+    },
+    navigationBarText: {
+      color: theme.palette.white
+    },
+  };
 
-  render() {
-    return (
-      <div className="app">
-        <div className="navigation-bar">
-          <div className="navigation-bar-title">
-            Lark Designer
-          </div>
-
-          <div className="navigation-bar-menu">
-            { `Version ${this.props.version}` }
-          </div>
-        </div>
-
-        <div className="app-content">
-          <Pivot>
-            <PivotItem
-              headerText="Develop"
-              itemIcon="CodeEdit"
-            >
-              <input
-                id="grammar-file-input"
-                type="file"
-                accept=".lark"
-                onChange={(e) => this._loadGrammar(e)}
-              />
-              <CommandBar items={this._developCommandItems} />
-
-              {this.state.grammar.error 
-                ? <MessageBar
-                  messageBarType={MessageBarType.error}
-                  dismissButtonAriaLabel="Close"
-                >
-                  {this.state.grammar.error.message}
-                </MessageBar>
-                : ''
-              }
-
-              <TextField multiline value={this.state.grammar.text} />
-            </PivotItem>
-
-            <PivotItem
-              headerText="Test"
-              itemIcon="TestBeaker"
-            >
-              <div id="test-tab" className="tab-content">
-
-              </div>
-            </PivotItem>
-          </Pivot>
-        </div>
-      </div>
-    );
-  }
-
-  _loadGrammar(evt) {
-    const file = evt.target.files[0];
+  const loadGrammar = (file) => {
     const reader = new FileReader();
     reader.onload = (e) => {
-      this.setState({
-        ...this.state,
-        grammar: {
-          ...this.state.grammar,
+      setGrammar(prev => {
+        return {
+          ...prev,
           text: e.target.result
-        }
-      })
+        };
+      });
     };
     reader.readAsText(file, "UTF-8");
-  }
+  };
 
-  _promptGrammarFile() {
+  const promptGrammarFile = () => {
     const fileInput = document.getElementById("grammar-file-input");
     fileInput.click();
   }
 
-  async _validateGrammar() {
+  const renderGrammarError = (error) => {
+    if (!error) {
+      return '';
+    }
+    
+    return (
+      <MessageBar
+        messageBarType={MessageBarType.error}
+        dismissButtonAriaLabel="Close"
+      >
+        {error.message}
+      </MessageBar>
+    );
+  };
+
+  const renderValidationStatus = (grammar) => {
+    if (grammar.isValidating === undefined) {
+      return '';
+    }
+
+    if (grammar.isValidating) {
+      return (
+        <Spinner label="Validating..." labelPosition="right" /> 
+      );
+    } else if (grammar.isValid) {
+      return (
+        <Text nowrap>
+          <FontIcon iconName="CheckMark" /> Valid
+        </Text>
+      );
+    } else {
+      return (
+        <Text nowrap>
+          <FontIcon iconName="StatusCircleErrorX" /> Invalid
+        </Text>
+      );
+    }
+  };
+
+  const validateGrammar = async (text) => {
     try {
-      this.state.grammar.isValidating = true;
+      setGrammar(prev => {
+        return { 
+          ...prev,
+          isValidating: true 
+        };
+      });
 
       const res = await fetch("api/validate", {
         body: JSON.stringify({
-          grammar: this.state.grammar.text,
+          grammar: text,
         }),
         headers: {
           "Content-Type": "application/json",
@@ -118,34 +94,97 @@ class App extends React.Component {
       });
       const isSuccess = res.status >= 200 && res.status <= 299;
 
-      //this.state.grammar.isValid = isSuccess;
-      if (!this.state.grammar.isValid) {
+      if (!isSuccess) {
         const resBody = await res.json();
 
-        this.setState({
-          ...this.state,
-          grammar: {
-            ...this.state.grammar,
+        setGrammar(prev => {
+          return {
+            ...prev,
             error: {
               message: resBody.error.message.replaceAll("\n", "<br />"),
-            }
-          }
+            },
+            isValid: false
+          };
         });
       } else {
-        this.setState({
-          ...this.state,
-          grammar: {
-            ...this.state.grammar,
-            error: null
-          }
+        setGrammar(prev => {
+          return {
+            ...prev,
+            error: null,
+            isValid: true
+          };
         });
-        //this.state.grammar.error = null;
       }
     } finally {
-      //this.state.grammar.isValidating = false;
+      setGrammar(prev => {
+        return {
+          ...prev,
+          isValidating: false,
+        };
+      });
     }
-  }
-}
+  };
 
+  const _developCommandItems = [
+    {
+      key: 'load',
+      text: 'Load...',
+      iconProps: { iconName: 'Upload' },
+      onClick: () => promptGrammarFile()
+    },
+    {
+      key: 'validate',
+      text: 'Validate',
+      iconProps: { iconName: 'CheckMark' },
+      onClick: () => validateGrammar(grammar.text),
+    }
+  ];  
+
+  return (
+    <div>
+      <div className="navigation-bar" style={styles.navigationBar}>
+        <Text variant="large" style={styles.navigationBarText}>
+          Lark Designer
+        </Text>
+
+        <Text style={styles.navigationBarText}>
+          { `Version ${props.version}` }
+        </Text>
+      </div>
+
+      <Pivot>
+        <PivotItem
+          headerText="Develop"
+          itemIcon="CodeEdit"
+        >
+          <input
+            id="grammar-file-input"
+            type="file"
+            accept=".lark"
+            onChange={(e) => loadGrammar(e.target.files[0])}
+          />
+          <CommandBar items={_developCommandItems} />
+
+          {renderGrammarError(grammar.error)}
+
+          <TextField 
+            multiline 
+            className="code-input"
+            value={grammar.text} 
+            onChange={(e) => setGrammar({ text: e.target.value })}
+          />
+
+          {renderValidationStatus(grammar)}
+        </PivotItem>
+
+        <PivotItem
+          headerText="Test"
+          itemIcon="TestBeaker"
+        >
+        </PivotItem>
+      </Pivot>
+    </div>
+  );
+}
 
 export default App;
